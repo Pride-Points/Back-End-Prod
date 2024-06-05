95e413543c2173ef6b84bad1f7377fae90b73512
Enzo Medej <110927542+EnzoMedej@users.noreply.github.com>
2024-06-04 21:32:12 -0300

    vai build

--- src/main/java/com/pridepoints/api/security/NoSecurityConfig.java
+++ src/main/java/com/pridepoints/api/security/NoSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.security;
+package com.pridepoints.api.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
